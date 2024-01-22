2decbdeec4caf00a6117c71ebf25c744048067e8
punaywat-jar <punyawat0@gmail.com>
2024-01-22 12:31:27 +0700

    Final change

---
+++ index-v1.js
@@ -0,0 +1,57 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = "mongodb+srv://punyawat:M9tFEfh0WTgXSvAm@users.1bxfvsg.mongodb.net/users";
+const mongoose = require('mongoose');
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(url,{useNewUrlParser: true},{useUnifiedTopology : true},{serverApi: ServerApiVersion.v1})
+const notesSchema = {
+    name: String,
+    ex: String,
+    locate: String,
+    sex: String,
+    prefer: String
+}
+
+const Note = mongoose.model("Note",notesSchema);
+
+// const getSchema = new mongoose.Schema({
+//     name: String,
+//     ex: String,
+//     locate: String,
+//     sex: String
+// });
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+  
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.use(express.static(__dirname + '/Page'));
+
+app.get('/', function(req, res) {
+    res.render(path.join(__dirname, '/match.ejs'));
+});
+
+app.post("/",function(req,res){
+    let newNote = new Note({
+        name: req.body.username,
+        ex: req.body.user_ex,
+        locate: req.body.user_loc,
+        sex: req.body.user_sex,
+        prefer: req.body.user_prefer
+    })
+    newNote.save();
+    res.redirect('match.html')
+})
--- index.js
+++ index.js
@@ -1,57 +1,51 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const url = "mongodb+srv://punyawat:M9tFEfh0WTgXSvAm@users.1bxfvsg.mongodb.net/users";
-const mongoose = require('mongoose');
 
+//Server Express setting
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const path = require('path');
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-mongoose.connect(url,{useNewUrlParser: true},{useUnifiedTopology : true},{serverApi: ServerApiVersion.v1})
-const notesSchema = {
-    name: String,
-    ex: String,
-    locate: String,
-    sex: String,
-    prefer: String
-}
-
-const Note = mongoose.model("Note",notesSchema);
-
-// const getSchema = new mongoose.Schema({
-//     name: String,
-//     ex: String,
-//     locate: String,
-//     sex: String
-// });
-
 server.listen(3000, () => {
     console.log('listening on *:3000');
-  });
+});
+
+
+/// MySQL setting
+var mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'cvauUGGhFI',
+    password: 'DiVNwld5GM',
+    // database: 'cvauUGGhFI'
+});
   
+// con.connect((err) => {
+//     if(err){
+//       console.log('Error connecting to Db');
+//       return;
+//     }
+//     console.log('Connection established');
+// });
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+    });
+  });
+
+/// NodeJS setting
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.use(express.static(__dirname + '/Page'));
+app.post('/', function(req, res) {
 
-app.get('/', function(req, res) {
-    res.render(path.join(__dirname, '/match.ejs'));
+    res.sendFile(path.join(__dirname, '/match.html'));
 });
 
-app.post("/",function(req,res){
-    let newNote = new Note({
-        name: req.body.username,
-        ex: req.body.user_ex,
-        locate: req.body.user_loc,
-        sex: req.body.user_sex,
-        prefer: req.body.user_prefer
-    })
-    newNote.save();
-    res.redirect('match.html')
-})
+
